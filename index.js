db0092d002bce97768e86fa3b5c29c7e4c718f9f
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2023-10-19 13:44:05 -0400

    Neha (#3)
    * adding all the files
    
    * updating git ignore
    
    * final changes

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 
 const pulumi = require('@pulumi/pulumi');
 const aws = require('@pulumi/aws');
-
+const sdk=require("aws-sdk");
 
 const config = new pulumi.Config();
 const vpcCidr = config.require('vpcCidr');
 const vpcName = config.require('vpcName');
 const internetgateway=config.require('internetGateway')
-
-
+const keyNames=config.require('keyname')
+let ec2Instance;
+let AMI_ID;
+let AMMMID= config.require('amiId');
+let ownerId= config.require('ownerid')
 
 const vpc = new aws.ec2.Vpc(vpcName, {
     cidrBlock: vpcCidr,
@@ -37,6 +40,7 @@ const privateRouteTable = new aws.ec2.RouteTable('private-route-table', {
     vpcId: vpc.id,
     tags: { Name: 'private-route-table' },
 });
+
 
 const publicSubnets = [];
 const privateSubnets = [];
@@ -88,10 +92,80 @@ listAvailabilityZones().then((azs) => {
                   routeTableId: privateRouteTable.id,
         });
     }
-});
 
 
+}).then(()=>{
+
+
+        const appSecurityGroup = new aws.ec2.SecurityGroup("applicationSecurityGroup", {
+            vpcId: vpc.id,
+            ingress: [
+                {
+                    fromPort: 22,
+                    toPort: 22,
+                    protocol: "tcp",
+                    cidrBlocks: ["73.219.87.237/32"],
+                },
+                {
+                    fromPort: 80,
+                    toPort: 80,
+                    protocol: "tcp",
+                    cidrBlocks: ["0.0.0.0/0"],
+                },
+                {
+                    fromPort: 443,
+                    toPort: 443,
+                    protocol: "tcp",
+                    cidrBlocks: ["0.0.0.0/0"],
+                },
+                {
+                    fromPort: 8080,
+                    toPort: 8080,
+                    protocol: "tcp",
+                    cidrBlocks: ["0.0.0.0/0"],
+                },
+            ],
+        });
+      
+
+        const amiOwnerId = ownerId;
+        const awsConfig = new sdk.Config({ region: "us-east-1" }); 
+        const ec2 = new sdk.EC2(awsConfig);
+        
+        const listAmis = async () => {
+          const params = { Owners: ['ownerId'] };
+        
+          ec2.describeImages(params, function(err, data) {
+            if (err) console.log(err, err.stack);
+            else {
+              const amiIds = data.Images.map((ami) => ami.ImageId);
+              AMI_ID= amiIds;
+              console.log(AMI_ID[0])
+            }
+          });
+        };
+        
+        const amis = listAmis();
+        const instance = new aws.ec2.Instance("webAppInstance", {
+                ami: AMMMID,
+                instanceType: "t2.micro",
+                vpcSecurityGroupIds: [appSecurityGroup.id],
+                subnetId: publicSubnets[0].id,
+                keyName: keyNames,
+                rootBlockDevice: {
+                    volumeSize: 25,
+                    volumeType: "gp2",
+                    deleteOnTermination: true,
+                },
+                disableApiTermination: true,
+            });
+        
+    
+
+ec2Instance = instance; 
 exports.vpcId = vpc.id;
 exports.publicSubnetIds = publicSubnets.map(subnet => subnet.id);
 exports.privateSubnetIds = privateSubnets.map(subnet => subnet.id);
+exports.instanceId = instance.id;
 
+})
